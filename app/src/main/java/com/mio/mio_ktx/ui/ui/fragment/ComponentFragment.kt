a2eabfadef233d805e34806b8d1a110a4e3997da
nasamio <nasamio@163.com>
2024-04-09 15:15:45 +0800

    增加图片加载(圆角、圆形、模糊)功能

--- app/src/main/java/com/mio/mio_ktx/ui/ui/fragment/ComponentFragment.kt
+++ app/src/main/java/com/mio/mio_ktx/ui/ui/fragment/ComponentFragment.kt
@@ -2,9 +2,14 @@ package com.mio.mio_ktx.ui.ui.fragment
 
 import android.content.Intent
 import android.net.Uri
+import android.os.Build
 import com.mio.base.BaseFragment
 import com.mio.base.extension.bg
 import com.mio.base.extension.enablePressEffect
+import com.mio.base.extension.load
+import com.mio.base.extension.loadBlur
+import com.mio.base.extension.loadCircle
+import com.mio.base.extension.loadRound
 import com.mio.base.setClickListener
 import com.mio.mio_ktx.R
 import com.mio.mio_ktx.databinding.FragmentComponentBinding
@@ -16,30 +21,34 @@ class ComponentFragment : BaseFragment<FragmentComponentBinding>(R.layout.fragme
     }
 
     private fun testBgDrawable() {
-        mDataBinding.tv1.apply {
-            // 背景圆角
-            bg(mContext.getColor(com.mio.base.R.color.black_30), 5f, 5)
-            // 前景按压
-            enablePressEffect()
-        }
-        mDataBinding.btn1.apply {
-            // 背景圆角
-            bg(mContext.getColor(com.mio.base.R.color.black_30), 20f, 5)
+        val url = "https://t7.baidu.com/it/u=3601447414,1764260638&fm=193&f=GIF"
+        mDataBinding.ivTest.load(url)
 
-            setClickListener {
-                val sceneViewerIntent = Intent(Intent.ACTION_VIEW)
-                val intentUri = Uri.parse("https://arvr.google.com/scene-viewer/1.0").buildUpon()
-                    .appendQueryParameter(
-                        "file",
-                        "https://raw.githubusercontent.com/KhronosGroup/glTF-Sample-Models/master/2.0/Avocado/glTF/Avocado.gltf"
-                    )
-                    .appendQueryParameter("mode", "ar_only")
-                    .build()
-                sceneViewerIntent.setData(intentUri)
-                sceneViewerIntent.setPackage("com.google.ar.core")
-                // startActivity(sceneViewerIntent)
-            }
-        }
+
+//        mDataBinding.tv1.apply {
+//            // 背景圆角
+//            bg(mContext.getColor(com.mio.base.R.color.black_30), 5f, 5)
+//            // 前景按压
+//            enablePressEffect()
+//        }
+//        mDataBinding.btn1.apply {
+//            // 背景圆角
+//            bg(mContext.getColor(com.mio.base.R.color.black_30), 20f, 5)
+//
+//            setClickListener {
+//                val sceneViewerIntent = Intent(Intent.ACTION_VIEW)
+//                val intentUri = Uri.parse("https://arvr.google.com/scene-viewer/1.0").buildUpon()
+//                    .appendQueryParameter(
+//                        "file",
+//                        "https://raw.githubusercontent.com/KhronosGroup/glTF-Sample-Models/master/2.0/Avocado/glTF/Avocado.gltf"
+//                    )
+//                    .appendQueryParameter("mode", "ar_only")
+//                    .build()
+//                sceneViewerIntent.setData(intentUri)
+//                sceneViewerIntent.setPackage("com.google.ar.core")
+//                // startActivity(sceneViewerIntent)
+//            }
+//        }
     }
 
     override fun initObserver() {
